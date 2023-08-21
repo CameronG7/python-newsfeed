055d86ce6b02a886e4e95b2dc84f7af455a9630c
CameronG7 <122698132+CameronG7@users.noreply.github.com>
2023-08-21 15:55:05 -0700

    homepage

--- app/models/Post.py
+++ app/models/Post.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 from app.db import Base
 from .Vote import Vote
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, select
-from sqlalchemy.sql import func
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, select, func
+
+from sqlalchemy.orm import relationship, column_property
+
 
 class Post(Base):
     __tablename__ = 'posts'
@@ -16,20 +16,14 @@ class Post(Base):
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
     user = relationship('User')
     comments = relationship('Comment', cascade='all,delete')
-    # vote_count = column_property(
-    #     select([func.count(Vote.id)]).where(Vote.post_id == id)
-    # )
+    vote_count = column_property(
+        select((func.count(Vote.id))).where(Vote.post_id == id)
+    )
     votes = relationship('Vote', cascade='all,delete')
+    
 
     def __repr__(self):
         return f'Post: {self.title}'
-    # this is the way to do add  a column property in sqlalchemy 2.0 
-    @hybrid_property
-    def vote_count(self):
-        return self._vote_count
     
-    @vote_count.expression
-    def _vote_count(cls):
-        return select([func.count(Vote.id)]).where(Vote.post_id == cls.id).label('vote_count')
     
     
